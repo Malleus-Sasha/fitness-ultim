b2eaf3d2c9d771223a1b52b3fcfe1c94c90ed82b
Alex Masliuk <oleksandrm@suntechinnovation.com>
2021-02-08 19:12:01 +0200

    FITULT: HealthModule setup and lazy loading.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,23 @@ import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 
 import { Store } from 'store';
+import { HealthModule } from './health/health.module';
+import { ScheduleModule } from './health/schedule/schedule.module';
+import { WorkoutsModule } from './health/workouts/workouts.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule,
+    HealthModule,
+    ScheduleModule,
+    WorkoutsModule,
   ],
   providers: [
     Store
---
+++ src/app/health/meals/meals.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MealsComponent } from './containers/meals/meals.component';
+
+const ROUTES: Routes = [
+  { path: '', component: MealsComponent }
+]
+
+
+@NgModule({
+  declarations: [
+    MealsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+    ReactiveFormsModule
+  ]
+})
+export class MealsModule { }
